bdae7afeb1bd786e9450e0982a98afea96aaddd2
dungphan294 <dungpv.294@gmail.com>
2024-01-08 21:02:31 +0100

    Demo code update

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -14,13 +14,171 @@ const int BCD_D_PIN = A3;
 
 // ROTARY ENCODER
 const int RE_BUTTON_PIN = 6;    // SW
-const int RE_CLK_PIN = 5;       // A
-const int RE_DATA_PIN = 4;      // B
+const int RE_CLK_PIN = 5;       // BCD_A_PIN
+const int RE_DATA_PIN = 4;      // BCD_B_PIN
 #define DIRECTION_CW 0          // clockwise direction
 #define DIRECTION_CCW 1         // counter-clockwise direction
 
 // -- VARIABLES
 const int DEBOUNCE_TIME = 50;
+
+
+void zero() {  //0000
+    digitalWrite(BCD_A_PIN, LOW);
+    digitalWrite(BCD_B_PIN, LOW);
+    digitalWrite(BCD_C_PIN, LOW);
+    digitalWrite(BCD_D_PIN, LOW);
+}
+
+void one() {  //0001
+    digitalWrite(BCD_A_PIN, HIGH);
+    digitalWrite(BCD_B_PIN, LOW);
+    digitalWrite(BCD_C_PIN, LOW);
+    digitalWrite(BCD_D_PIN, LOW);
+}
+
+void two() {  // 0010
+    digitalWrite(BCD_A_PIN, LOW);
+    digitalWrite(BCD_B_PIN, HIGH);
+    digitalWrite(BCD_C_PIN, LOW);
+    digitalWrite(BCD_D_PIN, LOW);
+}
+
+void three() {  // 0011
+    digitalWrite(BCD_A_PIN, HIGH);
+    digitalWrite(BCD_B_PIN, HIGH);
+    digitalWrite(BCD_C_PIN, LOW);
+    digitalWrite(BCD_D_PIN, LOW);
+}
+
+void four() {  // 0100
+    digitalWrite(BCD_A_PIN, LOW);
+    digitalWrite(BCD_B_PIN, LOW);
+    digitalWrite(BCD_C_PIN, HIGH);
+    digitalWrite(BCD_D_PIN, LOW);
+}
+
+void five() {  // 0101
+    digitalWrite(BCD_A_PIN, HIGH);
+    digitalWrite(BCD_B_PIN, LOW);
+    digitalWrite(BCD_C_PIN, HIGH);
+    digitalWrite(BCD_D_PIN, LOW);
+}
+
+void six() {  // 0110
+    digitalWrite(BCD_A_PIN, LOW);
+    digitalWrite(BCD_B_PIN, HIGH);
+    digitalWrite(BCD_C_PIN, HIGH);
+    digitalWrite(BCD_D_PIN, LOW);
+}
+
+void seven() {  // 0111
+    digitalWrite(BCD_A_PIN, HIGH);
+    digitalWrite(BCD_B_PIN, HIGH);
+    digitalWrite(BCD_C_PIN, HIGH);
+    digitalWrite(BCD_D_PIN, LOW);
+}
+
+void eight() {  // 1000
+    digitalWrite(BCD_A_PIN, LOW);
+    digitalWrite(BCD_B_PIN, LOW);
+    digitalWrite(BCD_C_PIN, LOW);
+    digitalWrite(BCD_D_PIN, HIGH);
+}
+
+void nine() {  // 1001
+    digitalWrite(BCD_A_PIN, HIGH);
+    digitalWrite(BCD_B_PIN, LOW);
+    digitalWrite(BCD_C_PIN, LOW);
+    digitalWrite(BCD_D_PIN, HIGH);
+}
+
+
+void displayNumber(int number, int display) {
+    static int shift_register_1 = 0;
+//    static int shift_register_2 = 0;
+    byte patterns[10] = {
+            B01000000,  // 0
+            B01111001,  // 1
+            B00100100,  // 2
+            B00110000,  // 3
+            B00011001,  // 4
+            B00010010,  // 5
+            B00000011,  // 6
+            B11111000,  // 7
+            B00000000,  // 8
+            B00011000   // 9
+    };
+    // TODO: Function to control BCD
+    if (display == 0) { // Choosing value for the first shift register
+        // Shift out the pattern to the first 74HC595
+        digitalWrite(SR_LATCH_PIN, LOW);
+        shiftOut(SR_DATA_PIN, SR_CLOCK_PIN, MSBFIRST, patterns[0]); // Clear the second shift register
+        shiftOut(SR_DATA_PIN, SR_CLOCK_PIN, MSBFIRST, patterns[number]); // Shift pattern for the first display
+        digitalWrite(SR_LATCH_PIN, HIGH);
+        shift_register_1 = number;
+    } else if (display == 1) { // Choosing value for the second shift register
+        // Shift out the pattern to the second 74HC595
+        digitalWrite(SR_LATCH_PIN, LOW);
+        shiftOut(SR_DATA_PIN, SR_CLOCK_PIN, MSBFIRST, patterns[number]);
+        shiftOut(SR_DATA_PIN, SR_CLOCK_PIN, MSBFIRST, patterns[shift_register_1]); // Save first number
+        digitalWrite(SR_LATCH_PIN, HIGH);
+//        shift_register_2 = number;
+    } else if (display == 2) {
+        if (number == 0) {
+            digitalWrite(BCD_A_PIN, LOW);
+            digitalWrite(BCD_B_PIN, LOW);
+            digitalWrite(BCD_C_PIN, LOW);
+            digitalWrite(BCD_D_PIN, LOW);
+        } else if (number == 1) {
+            digitalWrite(BCD_A_PIN, HIGH);
+            digitalWrite(BCD_B_PIN, LOW);
+            digitalWrite(BCD_C_PIN, LOW);
+            digitalWrite(BCD_D_PIN, LOW);
+        } else if (number == 2) {
+            digitalWrite(BCD_A_PIN, LOW);
+            digitalWrite(BCD_B_PIN, HIGH);
+            digitalWrite(BCD_C_PIN, LOW);
+            digitalWrite(BCD_D_PIN, LOW);
+        } else if (number == 3) {
+            digitalWrite(BCD_A_PIN, HIGH);
+            digitalWrite(BCD_B_PIN, HIGH);
+            digitalWrite(BCD_C_PIN, LOW);
+            digitalWrite(BCD_D_PIN, LOW);
+        } else if (number == 4) {
+            digitalWrite(BCD_A_PIN, LOW);
+            digitalWrite(BCD_B_PIN, LOW);
+            digitalWrite(BCD_C_PIN, HIGH);
+            digitalWrite(BCD_D_PIN, LOW);
+        } else if (number == 5) {
+            digitalWrite(BCD_A_PIN, HIGH);
+            digitalWrite(BCD_B_PIN, LOW);
+            digitalWrite(BCD_C_PIN, HIGH);
+            digitalWrite(BCD_D_PIN, LOW);
+        } else if (number == 6) {
+            digitalWrite(BCD_A_PIN, LOW);
+            digitalWrite(BCD_B_PIN, HIGH);
+            digitalWrite(BCD_C_PIN, HIGH);
+            digitalWrite(BCD_D_PIN, LOW);
+        } else if (number == 7) {
+            digitalWrite(BCD_A_PIN, HIGH);
+            digitalWrite(BCD_B_PIN, HIGH);
+            digitalWrite(BCD_C_PIN, HIGH);
+            digitalWrite(BCD_D_PIN, LOW);
+        } else if (number == 8) {
+            digitalWrite(BCD_A_PIN, LOW);
+            digitalWrite(BCD_B_PIN, LOW);
+            digitalWrite(BCD_C_PIN, LOW);
+            digitalWrite(BCD_D_PIN, HIGH);
+        } else if (number == 9) {
+            digitalWrite(BCD_A_PIN, HIGH);
+            digitalWrite(BCD_B_PIN, LOW);
+            digitalWrite(BCD_C_PIN, LOW);
+            digitalWrite(BCD_D_PIN, HIGH);
+        }
+    }
+}
+
 
 void setup() {
     Serial.begin(9600);
@@ -38,6 +196,9 @@ void setup() {
     pinMode(RE_CLK_PIN, INPUT);
     pinMode(RE_DATA_PIN, INPUT);
 
+    displayNumber(0, 0);
+    displayNumber(0, 1);
+    displayNumber(0, 2);
 }
 
 void loop() {
@@ -49,8 +210,8 @@ void loop() {
      * TODO:    IF LOW then -> switch to the second element on the array, and so on.
      * TODO: IF (
      */
-    //const int seven_segments[3] = {0, 1, 2};
-    //static int index_seven_segments = 0;
+    const int seven_segments[3] = {0, 1, 2};
+    static int index_seven_segments = 0;
     static int counter = 0;
     static bool direction = DIRECTION_CW;
 
@@ -62,9 +223,8 @@ void loop() {
     // ROTARY ENCODER CLOCK
     static int prev_RE_CLK_state = HIGH;
     const int RE_CLK_state = digitalRead(RE_CLK_PIN);
-    // ROTARY ENCODER DATA
 
-//    // TODO: Check RE_BUTTON click or not
+
     if (RE_BUTTON_state != prev_RE_BUTTON_state && (current_time - time_of_last_change > DEBOUNCE_TIME)) {
         // a valid change was detected
         time_of_last_change = current_time;         // save time of this change
@@ -72,12 +232,9 @@ void loop() {
         if (RE_BUTTON_state == LOW) {
             // Switch two the second seven segment
             Serial.println("The button is pressed");
+            index_seven_segments++;
         }
-        // TODO: RE value displays on first seven segment
     }
-
-    // read the current state of the rotary encoder's CLK pin
-
     // If the state of CLK is changed, then pulse occurred
     // React to only the rising edge (from LOW to HIGH) to avoid double count
     if (RE_CLK_state != prev_RE_CLK_state && RE_CLK_state == HIGH &&
@@ -103,10 +260,13 @@ void loop() {
             Serial.print("Clockwise");
         else
             Serial.print("Counter-clockwise");
-
         Serial.print(" | COUNTER: ");
         Serial.println(counter);
+        displayNumber(counter, seven_segments[index_seven_segments]);
     }
     // save last CLK state
     prev_RE_CLK_state = RE_CLK_state;
+
 }
+
+
